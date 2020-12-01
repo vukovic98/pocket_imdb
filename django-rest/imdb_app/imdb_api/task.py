3700adf959234e79b7e04d2077e5845ed390f1b4
Vladimir <vladimirvukovic98@hotmail.rs>
2020-12-01 12:35:53 +0100

    Task for mail on every hour

--- django-rest/imdb_app/imdb_api/task.py
+++ django-rest/imdb_app/imdb_api/task.py
@@ -1,5 +1,8 @@
 from celery import shared_task
+from celery.task import periodic_task
+from celery.schedules import crontab  
 from django.core.mail import send_mail
+from datetime import timedelta
 from django.conf import settings
 
 @shared_task(retry_kwargs={'max_retries': 3, 'countdown': 5})
@@ -12,6 +15,23 @@ def send_update_mail(title):
             [settings.ADMIN_EMAIL_ADDRESS],
             fail_silently=False
         )
-    except(Exception, exc):
-        raise send_update_mail.retry(exc=exc)
-    return True
+        return True
+    except Exception as e:
+        try:
+            send_update_mail.retry()
+        except MaxRetriesExceededError:
+            retry_email_1h(title)
+
+@periodic_task(run_every=timedelta(hours=1), options={"task_id":"12345"})
+def retry_email_1h(title):
+    send_mail(
+        'Information Mail',
+        'A new movie ' + title + ' was created!',
+        'from@example.com',
+        [settings.ADMIN_EMAIL_ADDRESS],
+        fail_silently=False
+    )
+    stop()
+
+def stop():
+    x = revoke("12345",terminate=True,signal="KILL")
